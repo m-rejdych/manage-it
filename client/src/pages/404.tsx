e8a2ed53e5ee5a19df41b411e050022c4b838b06
m-rejdych <rejdych.m.r@gmail.com>
2021-07-11 00:17:23 +0200

    Added 404 page.

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Stack, Box, Typography, Divider } from '@material-ui/core';
+
+const NotFound: React.FC = () => (
+  <Box height="100vh" display="flex" alignItems="center" justifyContent="center">
+    <Stack spacing={2} direction="row" divider={<Divider orientation="vertical" flexItem />}>
+      <Typography variant="h4">400</Typography>
+      <Typography variant="h6">Page not found.</Typography>
+    </Stack>
+  </Box>
+);
+
+export default NotFound;
